405f64847c1ae0bb0a3b7959883ae943849f1a9c
Billie Simmons <billiesnyc@gmail.com>
2018-06-02 16:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var theBeatlesPlay = function(musicians, instruments) {
   var emptyArray = [];
 for (i = 1; i < musicians.length; i++) {
-  emptyArray.push(musicians + "plays " + instruments)
+  emptyArray.push(musicians + "plays " + instruments);
 }
 
 }
